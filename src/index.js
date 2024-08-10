a0a5250e3d5ba963aae7e5e8e6d7dad195a5824d
Paviel Barysevich-Bleyaniuk <pavelmessage@gmail.com>
2024-08-10 11:47:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  return Number(Math.abs(n).toString().split('').reverse().join(''));
+//OR
+//   let absN = Math.abs(n);
+//   let reversedN = '';
+
+//   for (let i = absN.length - 1; i >= 0; i--) {
+//     reversedN += absN[i];
+//   }
+
+//   return +reversedN;
 }
